78599895aa7e86841fde4b95e6849ff058a789f2
Xazkiel0 <xazkiel@gmail.com>
2024-05-29 02:32:51 -0700

    init expo

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
+
